c9b29f49ca8897786627f274453387823ff68f09
Mark Plant <gothicsyn@live.co.uk>
2022-09-10 16:46:55 +0100

    Began work on 2022 updates

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource[] sfx;
+    public AudioSource[] bgm;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
